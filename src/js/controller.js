5ec5eb93a05c2bed22050057dd7f5e46185d4d5f
arturogarciam <arturo.gm888@gmail.com>
2021-03-28 22:18:54 -0600

    no cls

--- src/js/controller.js
+++ src/js/controller.js
@@ -140,7 +140,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
   newFeature();
 };
 init();
