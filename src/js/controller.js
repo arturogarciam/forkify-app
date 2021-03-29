c3690107d15fa5a382925f45e03628150eb0f154
arturogarciam <arturo.gm888@gmail.com>
2021-03-28 21:35:36 -0600

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -128,6 +128,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -137,5 +141,6 @@ const init = function () {
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
   console.log('Welcome!');
+  newFeature();
 };
 init();
